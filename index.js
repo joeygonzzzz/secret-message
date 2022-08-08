71f0a83f356a2dbd49b6b2e398e857389b75cdf7
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-08-07 22:48:36 -0500

    Using the btoa function to make shared message encrypted

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hi")
+document.querySelector('form').addEventListener('submit', event => {
+    event.preventDefault();
+
+    const input = document.querySelector('#message-input');
+    const encrypted = btoa(input.value);
+
+    document.querySelector('#link-input').value = encrypted
+});
